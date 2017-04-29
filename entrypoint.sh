1be92bd32d885b2db7190058f5ed251136043a53
zeva-dev <zeva.dev@gmail.com>
2017-04-29 15:04:57 +1000

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,42 +6,42 @@ mkdir -p /etc/ddclient/
 mkdir -p /var/cache/ddclient/
 ln -s /etc/ddclient.conf /etc/ddclient/ddclient.conf
 
-: "${DDS_DAEMON_OR_ONESHOT?Need to set DDS_DAEMON_OR_ONESHOT env var}"
-: "${DDS_SERVER?Need to set DDS_SERVER env var}"
-: "${DDS_USERNAME?Need to set DDS_USERNAME env var}"
-: "${DDS_PASSWORD?Need to set DDS_PASSWORD env var}"
-: "${DDS_DOMAIN?Need to set DDS_DOMAIN env var}"
-: "${DDS_AUTOPUBLIC_OR_INTERFACE?Need to set DDS_AUTOPUBLIC_OR_INTERFACE env var, chose autopublic, or a proper interface name such as eth0}"
+: "${DDNS_DAEMON_OR_ONESHOT?Need to set DDNS_DAEMON_OR_ONESHOT env var}"
+: "${DDNS_SERVER?Need to set DDNS_SERVER env var}"
+: "${DDNS_USERNAME?Need to set DDNS_USERNAME env var}"
+: "${DDNS_PASSWORD?Need to set DDNS_PASSWORD env var}"
+: "${DDNS_DOMAIN?Need to set DDNS_DOMAIN env var}"
+: "${DDNS_AUTOPUBLIC_OR_INTERFACE?Need to set DDNS_AUTOPUBLIC_OR_INTERFACE env var, chose autopublic, or a proper interface name such as eth0}"
 
-if  [ "$DDS_AUTOPUBLIC_OR_INTERFACE" = "autopublic" ]
+if  [ "$DDNS_AUTOPUBLIC_OR_INTERFACE" = "autopublic" ]
 then
     echo "use=web, web=checkip.dyndns.com/, web-skip='IP Address'" >> "${CONF_FILE}"
     #echo "use=web, web=checkip.dynu.com/, web-skip='IP Address'" >>  "${CONF_FILE}"
 else
     INTERFACE_LIST=`cat /proc/net/dev | grep ":" | awk -F ":" '{print $1}' | awk '{$1=$1;print $1}'`
-    result=`echo "${INTERFACE_LIST}" | grep "^${DDS_AUTOPUBLIC_OR_INTERFACE}$" | wc -l`
+    result=`echo "${INTERFACE_LIST}" | grep "^${DDNS_AUTOPUBLIC_OR_INTERFACE}$" | wc -l`
     if ! [ "${result}" = "1" ]
     then
-        echo "DDS_AUTOPUBLIC_OR_INTERFACE should be set to autopublic or to one of the available interfaces"
+        echo "DDNS_AUTOPUBLIC_OR_INTERFACE should be set to autopublic or to one of the available interfaces"
         echo "$INTERFACE_LIST"
         exit
     fi
-    echo "use=if, if=${DDS_AUTOPUBLIC_OR_INTERFACE}" >>  "${CONF_FILE}"
+    echo "use=if, if=${DDNS_AUTOPUBLIC_OR_INTERFACE}" >>  "${CONF_FILE}"
 fi
 
-sed -i "s/SERVER/${DDS_SERVER}/g" ${CONF_FILE}
-sed -i "s/USERNAME/${DDS_USERNAME}/g" ${CONF_FILE}
-sed -i "s/PASSWORD/${DDS_PASSWORD}/g" ${CONF_FILE}
-echo "${DDS_DOMAIN}" >>  "${CONF_FILE}"
+sed -i "s/SERVER/${DDNS_SERVER}/g" ${CONF_FILE}
+sed -i "s/USERNAME/${DDNS_USERNAME}/g" ${CONF_FILE}
+sed -i "s/PASSWORD/${DDNS_PASSWORD}/g" ${CONF_FILE}
+echo "${DDNS_DOMAIN}" >>  "${CONF_FILE}"
 
-if  [ "$DDS_DAEMON_OR_ONESHOT" = "daemon" ]
+if  [ "$DDNS_DAEMON_OR_ONESHOT" = "daemon" ]
 then
     echo "daemon=60" >> ${CONF_FILE}
     ddclient -foreground -verbose -daemon=30
-elif [ "$DDS_DAEMON_OR_ONESHOT" = "oneshot" ]
+elif [ "$DDNS_DAEMON_OR_ONESHOT" = "oneshot" ]
 then
     ddclient -foreground -verbose -daemon=0
 else
-    echo "DDS_DAEMON_OR_ONESHOT should be set to daemon or to oneshot"
+    echo "DDNS_DAEMON_OR_ONESHOT should be set to daemon or to oneshot"
 fi
 
